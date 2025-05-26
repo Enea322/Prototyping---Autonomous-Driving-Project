44492f99d4fa3742ba32ca0015a0e70090b1a305
Moaz Bin Alamgir Chowdhury <moaz-bin-alamgir.chowdhury@stud.hshl.de>
2025-05-26 13:49:57 +0200

    Updated ir_module.ino
    Modified the IR sensor code so that the sensor could be tested independently.

--- Sensor Modules/ir_module/ir_module.ino
+++ Sensor Modules/ir_module/ir_module.ino
@@ -3,25 +3,29 @@
 #define IR_RIGHT_PIN 8   // Right IR sensor OUT
 
 //IR Sensor Initialization
-void initIR() {
+void setup() {
   pinMode(IR_LEFT_PIN, INPUT);
   pinMode(IR_RIGHT_PIN, INPUT);
+  Serial.begin(9600);
 }
 
-//Check if line is detected under either sensor
-bool isLineDetected() {
+void loop() {
   int left = digitalRead(IR_LEFT_PIN);
   int right = digitalRead(IR_RIGHT_PIN);
+  
+  if (left == LOW || right == LOW);{
+    Serial.println("Bothe sensor under line");
+  }
+  else if (left == LOW);{
+    Serial.println("Left sensor under line");
+  }
+  else if (right == LOW);{
+    Serial.println("Right sensor under line");
+  }
+  else {
+    Serial.println("Line is not detected");
+  }
+  
+  delay(200);  
 
-  //Black line: LOW reading when detected
-  return (left == LOW || right == LOW);
-}
-
-//Check if both sensors are off the line
-bool isEndOfLine() {
-  int left = digitalRead(IR_LEFT_PIN);
-  int right = digitalRead(IR_RIGHT_PIN);
-
-  // Both HIGH: white surface; no line detected
-  return (left == HIGH && right == HIGH);
 }
