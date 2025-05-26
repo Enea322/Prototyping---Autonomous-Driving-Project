c61db642b7970b8d0c2d9ffef32674cb184913c0
fdiekenbrock <168469394+fdiekenbrock@users.noreply.github.com>
2025-05-26 13:25:33 +0200

    Update color_detection.ino
    Changed color detection algorithm to only detect red, as it is the most stable/verifiable measurement.

--- Sensor Modules/Color Sensor/color_detection.ino
+++ Sensor Modules/Color Sensor/color_detection.ino
@@ -8,16 +8,16 @@
 #define sensorOut 10
 /*Calibration values (must be updated before
 updated before each use)*/
+int t = 0;
 int redMin = 0;
 int redMax = 1;
-int greenMin = 0;
-int greenMax = 1;
 int redColor = 0;
-int greenColor = 0;
 int redFrequency = 0;
+int redAvg = 0;
 int redEdgeTime = 0;
-int greenFrequency = 0;
-int greenEdgeTime = 0;
+float redSum = 0;
+bool red = false;
+
 void setup()
 {
 /*definition of the sensor pins*/
@@ -37,54 +37,36 @@ Serial.begin(9600);
 //Initialization of the loop
 void loop() {
 Serial.println("------------------------------");
-{
+for(t=0,redFrequency=0;t<=10;t++){
 /*Determination of the photodiode type during measurement
 S2/S3
 LOW/LOW=RED, LOW/HIGH=BLUE,
 HIGH/HIGH=GREEN, HIGH/LOW=CLEAR*/
 digitalWrite(S2, LOW);
 digitalWrite(S3, LOW);
-/*Frequency measurement of the specified color and its as-
-signment to an RGB value between 0-255*/
-float(redEdgeTime) = pulseIn(sensorOut, HIGH) + pulseIn
-(sensorOut, LOW);
+/*Frequency measurement of the red color*/
+float(redEdgeTime) = pulseIn(sensorOut, HIGH) + pulseIn(sensorOut, LOW);
 float(redFrequency) = (1 / (redEdgeTime / 1000000));
-redColor = map(redFrequency, redMax, redMin, 255, 0);
-if (redColor > 255) {
-redColor = 255;
+redSum += redFrequency;
 }
-if (redColor < 0) {
-redColor = 0;
-}
-/*Output of frequency mapped to 0-255*/
-Serial.print("Red Frequency: ");
-Serial.println(redFrequency);
-Serial.print("R = ");
-Serial.println(redColor);
-delay(100);
-}
-{
-/*Output of frequency mapped to 0-255*/
-Serial.print("Green Frequency: ");
-Serial.println(greenFrequency);
-Serial.print("G = ");
-Serial.println(greenColor);
-delay(100);
-}
+redAvg = redSum/10;
+  if(redAvg > 700){
+    red = true;
+  }
+  else{
+    red = false;
+  }
+Serial.print("Average red detected: ");
+Serial.println(redAvg);
 Serial.println("------------------------------");
 delay(100);
-/*Determination of the measured color by comparison
-with the values of the other measured colors*/
-if (redColor > 700) {
+/*Determination of the measured color by comparison with the ambient average*/
+if (red == true) {
 Serial.println("Red detected ");
 delay(100);
 }
-if (700 > redColor) {
+if (red == false) {
 Serial.println("Green detected ");
-delay(100);
-}
-if (greenColor > 0 and redColor > 0) {
-Serial.println("Please place an Object in front of the Sensor");
 delay(100);
 }
 {
