9d27796ceae2a6744cf03668acaa661998beef58
fdiekenbrock <168469394+fdiekenbrock@users.noreply.github.com>
2025-05-26 13:01:48 +0200

    Update color_detection.ino

--- Sensor Modules/Color Sensor/color_detection.ino
+++ Sensor Modules/Color Sensor/color_detection.ino
@@ -12,17 +12,12 @@ int redMin = 0;
 int redMax = 1;
 int greenMin = 0;
 int greenMax = 1;
-int blueMin = 0;
-int blueMax = 1;
 int redColor = 0;
 int greenColor = 0;
-int blueColor = 0;
 int redFrequency = 0;
 int redEdgeTime = 0;
 int greenFrequency = 0;
 int greenEdgeTime = 0;
-int blueFrequency = 0;
-int blueEdgeTime = 0;
 void setup()
 {
 /*definition of the sensor pins*/
@@ -69,21 +64,6 @@ Serial.println(redColor);
 delay(100);
 }
 {
-digitalWrite(S2, HIGH);
-digitalWrite(S3, HIGH);
-/*Frequency measurement of the specified color and its as-
-signment to an RGB value between 0-255*/
-float(greenEdgeTime) = pulseIn(sensorOut, HIGH) + pulseIn
-(sensorOut, LOW);
-float(greenFrequency) = (1 / (greenEdgeTime / 1000000));
-greenColor = map(greenFrequency, greenMax, greenMin, 255,
-0);
-if (greenColor > 255) {
-greenColor = 255;
-}
-if (greenColor < 0) {
-greenColor = 0;
-}
 /*Output of frequency mapped to 0-255*/
 Serial.print("Green Frequency: ");
 Serial.println(greenFrequency);
@@ -91,49 +71,24 @@ Serial.print("G = ");
 Serial.println(greenColor);
 delay(100);
 }
-{
-digitalWrite(S2, LOW);
-digitalWrite(S3, HIGH);
-/*Frequency measurement of the specified color and its as-
-signment to an RGB value between 0-255*/
-float(blueEdgeTime) = pulseIn(sensorOut, HIGH) + pulseIn
-(sensorOut, LOW);
-float(blueFrequency) = (1 / (blueEdgeTime / 1000000));
-blueColor = map(blueFrequency, blueMax, blueMin, 255, 0);
-if (blueColor > 255) {
-blueColor = 255;
-}
-if (blueColor < 0) {
-blueColor = 0;
-}
-/*Output of frequency mapped to 0-255*/
-Serial.print("Blue Frequency: ");
-Serial.println(blueFrequency);
-Serial.print("B = ");
-Serial.println(blueColor);
-delay(100);
-}
 Serial.println("------------------------------");
 delay(100);
 /*Determination of the measured color by comparison
 with the values of the other measured colors*/
-if (redColor > greenColor and redColor > blueColor) {
+if (redColor > 700) {
 Serial.println("Red detected ");
 delay(100);
 }
-if (greenColor > redColor and greenColor > blueColor) {
+if (700 > redColor) {
 Serial.println("Green detected ");
 delay(100);
 }
-if (blueColor > redColor and blueColor > greenColor) {
-Serial.println("Blue detected ");
+if (greenColor > 0 and redColor > 0) {
+Serial.println("Please place an Object in front of the Sensor");
 delay(100);
 }
-if (blueColor > 0 and greenColor > 0 and redColor > 0) {
-Serial.println("Please place an Object in front of the
-Sensor");
-delay(100);
-}
+{
 Serial.println("------------------------------");
 delay(1000);
+}
 }
