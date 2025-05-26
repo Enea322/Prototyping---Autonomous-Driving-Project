5f157fe0e8bc8822b88f142d4403dd711021331a
Moaz Bin Alamgir Chowdhury <moaz-bin-alamgir.chowdhury@stud.hshl.de>
2025-05-26 14:10:19 +0200

    Updated code for ir_module.ino
    I have updated some minor typos in the code.

--- Sensor Modules/ir_module/ir_module.ino
+++ Sensor Modules/ir_module/ir_module.ino
@@ -7,25 +7,24 @@ void setup() {
   pinMode(IR_LEFT_PIN, INPUT);
   pinMode(IR_RIGHT_PIN, INPUT);
   Serial.begin(9600);
+  delay(500);
 }
 
 void loop() {
   int left = digitalRead(IR_LEFT_PIN);
   int right = digitalRead(IR_RIGHT_PIN);
   
-  if (left == LOW || right == LOW);{
+  if (left == 0 || right == 0){
     Serial.println("Bothe sensor under line");
   }
-  else if (left == LOW);{
+  else if (left == 0 || right == 1){
     Serial.println("Left sensor under line");
   }
-  else if (right == LOW);{
+  else if (left == 1 || right == 0){
     Serial.println("Right sensor under line");
   }
   else {
     Serial.println("Line is not detected");
-  }
-  
-  delay(200);  
+  } 
 
 }
