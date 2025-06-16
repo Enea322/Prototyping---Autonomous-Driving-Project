7209ec626ebd3050df2eac0efb57c8f662715f49
Moaz Bin Alamgir Chowdhury <moaz-bin-alamgir.chowdhury@stud.hshl.de>
2025-06-16 10:49:37 +0200

    Updated full system code

--- Full System Test/AutonomousCar_FullSystem/AutonomousCar_FullSystem.ino
+++ Full System Test/AutonomousCar_FullSystem/AutonomousCar_FullSystem.ino
@@ -1,67 +1,64 @@
-// Combined Arduino Code v4
-// Includes: Line following, obstacle detection, color detection, and special maneuvers
-
 #include <Servo.h>
 
-// --- Pin Definitions ---
-
-// Ultrasonic Sensor
-#define trigPin 13
-#define echoPin 12
-
-// Servo Motor
-#define servoPin 11
-Servo servo;
-int angle = 0;
-int step = 5; // degrees per step
-
-// IR Sensors
 #define IR_SENSOR_RIGHT A0
 #define IR_SENSOR_LEFT A1
+#define MOTOR_SPEED 80
 
-// Motor Pins
+const int SAFE_DISTANCE = 15; // in cm
+const int SCAN_ANGLE_LEFT = 150;
+const int SCAN_ANGLE_RIGHT = 30;
+const int CENTER_ANGLE = 90;
+const int FORWARD_STEP = 200;
+const int TURN_ANGLE_DURATION = 600;
+const int SERVO_DELAY = 300;
+const int SERVO_RESET_DELAY = 200;
+const int LINE_RECHECK_DELAY = 50;
+const int UTURN_DURATION = 1200;
+
+// Ultrasonic sensor pins
+const int trigPin = 6;
+const int echoPin = 7;
+
+// Right motor pins
 int enableRightMotor = 2;
 int rightMotorPin1 = 7;
 int rightMotorPin2 = 5;
+
+// Left motor pins
 int enableLeftMotor = 3;
 int leftMotorPin1 = 9;
 int leftMotorPin2 = 8;
 
-// Color Sensor
-#define S0 A2
-#define S1 A3
-#define S2 A4
-#define S3 A5
-#define sensorOut 10
-
-// Motor speed and timings
-const int MOTOR_SPEED = 150;
-const int FORWARD_DURATION_SHORT = 1500;
-const int FORWARD_DURATION_LONG  = 2000;
-const int FORWARD_DURATION_UTURN = 3000;
-const int TURN_90_DURATION       = 600;
-const int UTURN_DURATION         = 1200;
-const int SERVO_DELAY            = 300;
-const int SERVO_RESET_DELAY      = 200;
-
-// Color Sensor Calibration
-int t = 0;
-int redFrequency = 0;
-int redAvg = 0;
-int redAmbient = 0;
-float redSum = 0;
+// Flags
 bool red = false;
-bool ambientChecked = false;
 
-// --- Setup ---
+// Servo object
+Servo servo;
+
+// Function Declarations
+float scanSide(bool left);
+void followPerimeter(bool goLeft);
+bool isLineCentered();
+long readDistance();
+void rotateMotor(int rightMotorSpeed, int leftMotorSpeed);
+void moveForward(int speed, int duration);
+void stopCar();
+void turnLeft();
+void turnRight();
+void uTurn();
+void lineFollow();
+
+// Setup
 void setup() {
   Serial.begin(9600);
+  servo.attach(10);
 
-  // Ultrasonic
+  pinMode(IR_SENSOR_LEFT, INPUT);
+  pinMode(IR_SENSOR_RIGHT, INPUT);
+
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
-  // Motor pins
   pinMode(enableRightMotor, OUTPUT);
   pinMode(rightMotorPin1, OUTPUT);
   pinMode(rightMotorPin2, OUTPUT);
@@ -69,183 +66,91 @@ void setup() {
   pinMode(leftMotorPin1, OUTPUT);
   pinMode(leftMotorPin2, OUTPUT);
 
-  // IR Sensors
-  pinMode(IR_SENSOR_RIGHT, INPUT);
-  pinMode(IR_SENSOR_LEFT, INPUT);
-
-  // Servo
-  servo.attach(servoPin);
-  delay(500);
-
-  // Color Sensor
-  pinMode(S0, OUTPUT);
-  pinMode(S1, OUTPUT);
-  pinMode(S2, OUTPUT);
-  pinMode(S3, OUTPUT);
-  pinMode(sensorOut, INPUT);
-  digitalWrite(S0, LOW);
-  digitalWrite(S1, HIGH);
-
   rotateMotor(0, 0);
+  servo.write(CENTER_ANGLE);
 }
 
-// --- Loop ---
+// Main Loop
 void loop() {
-  long distance = readDistance();
-  Serial.print("Distance: "); Serial.print(distance); Serial.println(" cm");
+  if (red) {
+    float leftClear = scanSide(true);
+    float rightClear = scanSide(false);
 
-  if (distance > 0 && distance <= 10) rotateMotor(0, 0);
-
-  int rightIRSensorValue = digitalRead(IR_SENSOR_RIGHT);
-  int leftIRSensorValue = digitalRead(IR_SENSOR_LEFT);
-
-  // Line following logic
-  if (rightIRSensorValue == LOW && leftIRSensorValue == LOW) {
-    rotateMotor(MOTOR_SPEED, MOTOR_SPEED);  // Go straight
-  }
-  else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW) {
-    rotateMotor(-MOTOR_SPEED, MOTOR_SPEED);  // Turn right
-  }
-  else if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH) {
-    rotateMotor(MOTOR_SPEED, -MOTOR_SPEED);  // Turn left
-  }
-  else {
-    rotateMotor(0, 0);  // Stop
-
-    Serial.println("------------------------------");
-    if (!ambientChecked) {
-      for (t = 0, redSum = 0; t <= 10; t++) {
-        digitalWrite(S2, LOW);
-        digitalWrite(S3, LOW);
-        int redEdgeTime = pulseIn(sensorOut, HIGH) + pulseIn(sensorOut, LOW);
-        redFrequency = (1 / (redEdgeTime / 1000000.0));
-        redSum += redFrequency;
-      }
-      redAmbient = redSum / 10;
-      ambientChecked = true;
+    if (rightClear > SAFE_DISTANCE && rightClear >= leftClear) {
+      followPerimeter(false); // Go right
+    } else if (leftClear > SAFE_DISTANCE) {
+      followPerimeter(true);  // Go left
+    } else {
+      uTurn();
     }
 
-    for (t = 0, redSum = 0; t <= 10; t++) {
-      digitalWrite(S2, LOW);
-      digitalWrite(S3, LOW);
-      int redEdgeTime = pulseIn(sensorOut, HIGH) + pulseIn(sensorOut, LOW);
-      redFrequency = (1 / (redEdgeTime / 1000000.0));
-      redSum += redFrequency;
-    }
-    redAvg = redSum / 10;
-    red = redAvg > redAmbient;
-
-    Serial.print("Average red detected: ");
-    Serial.println(redAvg);
-    delay(100);
-
-    if (red) {
-      Serial.println("Red detected");
-
-      bool leftClear = isLeftClear();
-      bool rightClear = isRightClear();
-
-      if (rightClear && !leftClear) {
-        turnRight90();
-        moveForward(MOTOR_SPEED, FORWARD_DURATION_SHORT);
-        turnLeft90();
-        moveForward(MOTOR_SPEED, FORWARD_DURATION_LONG);
-        turnLeft90();
-        moveUntilLine();
-      }
-      else if (!rightClear && leftClear) {
-        turnLeft90();
-        moveForward(MOTOR_SPEED, FORWARD_DURATION_SHORT);
-        turnRight90();
-        moveForward(MOTOR_SPEED, FORWARD_DURATION_LONG);
-        turnRight90();
-        moveUntilLine();
-      }
-      else {
-        uTurn();
-        moveForward(MOTOR_SPEED, FORWARD_DURATION_SHORT);
-        turnRight90();
-        moveForward(MOTOR_SPEED, FORWARD_DURATION_UTURN);
-        turnRight90();
-        moveForward(MOTOR_SPEED, FORWARD_DURATION_UTURN);
-        turnRight90();
-        moveUntilLine();
-      }
-      Serial.println("Line reacquired, resuming line following.");
-      ambientChecked = false;
-    }
-    else {
-      Serial.println("Green detected");
-    }
-    Serial.println("------------------------------");
-    delay(1000);
+    red = false; // Reset for next detection
+  } else {
+    lineFollow();
   }
-  delay(100);
 }
 
-// --- Function Definitions ---
+// Helper Functions
+float scanSide(bool left) {
+  int start = CENTER_ANGLE;
+  int end = left ? SCAN_ANGLE_LEFT : SCAN_ANGLE_RIGHT;
+  int step = left ? 5 : -5;
+
+  float maxDistance = 0;
+  for (int angle = start; angle != end; angle += step) {
+    servo.write(angle);
+    delay(SERVO_DELAY);
+    float dist = readDistance();
+    if (dist > maxDistance) maxDistance = dist;
+  }
+  servo.write(CENTER_ANGLE);
+  delay(SERVO_RESET_DELAY);
+  return maxDistance;
+}
+
+void followPerimeter(bool goLeft) {
+  while (true) {
+    float dist = readDistance();
+    if (dist < SAFE_DISTANCE) {
+      if (goLeft) turnLeft();
+      else turnRight();
+      delay(200);
+      stopCar();
+      continue;
+    }
+
+    moveForward(MOTOR_SPEED, FORWARD_STEP);
+    if (isLineCentered()) {
+      stopCar();
+      break;
+    }
+  }
+}
+
+bool isLineCentered() {
+  int left = digitalRead(IR_SENSOR_LEFT);
+  int right = digitalRead(IR_SENSOR_RIGHT);
+  return (left == HIGH && right == HIGH);
+}
+
 long readDistance() {
-  digitalWrite(trigPin, LOW); delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH); delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  long duration = pulseIn(echoPin, HIGH, 30000);
-  return duration * 0.034 / 2;
-}
-
-bool isLeftClear() {
-  servo.write(150);
-  delay(SERVO_DELAY);
-  long distance = readDistance();
-  servo.write(90);
-  delay(SERVO_RESET_DELAY);
-  return distance > 10;
-}
-
-bool isRightClear() {
-  servo.write(30);
-  delay(SERVO_DELAY);
-  long distance = readDistance();
-  servo.write(90);
-  delay(SERVO_RESET_DELAY);
-  return distance > 10;
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  long duration = pulseIn(echoPin, HIGH);
+  return duration / 58; // Convert to cm
 }
 
 void moveForward(int speed, int duration) {
   rotateMotor(speed, speed);
   delay(duration);
-  rotateMotor(0, 0);
-}
-
-void turnLeft90() {
-  rotateMotor(-MOTOR_SPEED, MOTOR_SPEED);
-  delay(TURN_90_DURATION);
-  rotateMotor(0, 0);
-}
-
-void turnRight90() {
-  rotateMotor(MOTOR_SPEED, -MOTOR_SPEED);
-  delay(TURN_90_DURATION);
-  rotateMotor(0, 0);
-}
-
-void uTurn() {
-  rotateMotor(MOTOR_SPEED, -MOTOR_SPEED);
-  delay(UTURN_DURATION);
-  rotateMotor(0, 0);
-}
-
-void moveUntilLine() {
-  while (true) {
-    int rightIR = digitalRead(IR_SENSOR_RIGHT);
-    int leftIR = digitalRead(IR_SENSOR_LEFT);
-    if (rightIR == LOW || leftIR == LOW) break;
-    rotateMotor(MOTOR_SPEED, MOTOR_SPEED);
-    delay(50);
-  }
-  rotateMotor(0, 0);
+  stopCar();
 }
 
 void rotateMotor(int rightMotorSpeed, int leftMotorSpeed) {
+  // Right motor control
   if (rightMotorSpeed < 0) {
     digitalWrite(rightMotorPin1, HIGH);
     digitalWrite(rightMotorPin2, LOW);
@@ -257,6 +162,7 @@ void rotateMotor(int rightMotorSpeed, int leftMotorSpeed) {
     digitalWrite(rightMotorPin2, HIGH);
   }
 
+  // Left motor control
   if (leftMotorSpeed < 0) {
     digitalWrite(leftMotorPin1, LOW);
     digitalWrite(leftMotorPin2, HIGH);
@@ -270,4 +176,41 @@ void rotateMotor(int rightMotorSpeed, int leftMotorSpeed) {
 
   analogWrite(enableRightMotor, abs(rightMotorSpeed));
   analogWrite(enableLeftMotor, abs(leftMotorSpeed));
+}
+
+void stopCar() {
+  rotateMotor(0, 0);
+}
+
+void turnLeft() {
+  rotateMotor(MOTOR_SPEED, -MOTOR_SPEED);
+  delay(TURN_ANGLE_DURATION);
+  stopCar();
+}
+
+void turnRight() {
+  rotateMotor(-MOTOR_SPEED, MOTOR_SPEED);
+  delay(TURN_ANGLE_DURATION);
+  stopCar();
+}
+
+void uTurn() {
+  rotateMotor(-MOTOR_SPEED, MOTOR_SPEED);
+  delay(UTURN_DURATION);
+  stopCar();
+}
+
+void lineFollow() {
+  int rightIRSensorValue = digitalRead(IR_SENSOR_RIGHT);
+  int leftIRSensorValue = digitalRead(IR_SENSOR_LEFT);
+
+  if (rightIRSensorValue == LOW && leftIRSensorValue == LOW) {
+    rotateMotor(MOTOR_SPEED, MOTOR_SPEED);  // Go straight
+  } else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW) {
+    rotateMotor(-MOTOR_SPEED, MOTOR_SPEED);  // Turn right
+  } else if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH) {
+    rotateMotor(MOTOR_SPEED, -MOTOR_SPEED);  // Turn left
+  } else {
+    rotateMotor(0, 0);  // Stop
+  }
 }
